397d6c1c762c44ae0bfdeeff8c00ecebdfee5585
Jon Taylor <jont@cakesolutions.net>
2017-06-12 16:00:12 +0100

    Added some UI

--- client/index.js
+++ client/index.js
@@ -6,20 +6,13 @@ const range = (start, len) => Array.from(Array(len).keys()).map(n => n + start);
 const max = (a, b) => Math.max(a, b);
 const reduceMax = xs => xs.reduce(max, 0);
 
-const startOptions = {
-    // debug: 'verbose'
-    maxThreads: 8,
-};
-hamsters.init(startOptions);
-
-var params = {
-    ['array']: range(0, ARRAY_LENGTH)
-};
-
 const doIt = maxThreads => {
     const timerName = `timer-${maxThreads}`;
     return new Promise((resolve, reject) => {
-        console.time(timerName);
+        const start = Date.now()
+        const params = {
+            ['array']: range(0, ARRAY_LENGTH)
+        };
         hamsters.run(
             params,
             function f1() {
@@ -34,8 +27,8 @@ const doIt = maxThreads => {
             },
             function f2() {
                 const result = reduceMax(arguments[0]);
-                console.log(`result (${maxThreads}): ${result}`);
-                console.timeEnd(timerName);
+                const end = Date.now();
+                log(`[${maxThreads} threads]: result ${result}; elapsed time (ms): ${end - start}`);
                 resolve(result);
             },
             maxThreads,
@@ -44,6 +37,49 @@ const doIt = maxThreads => {
     });
 };
 
-doIt(2)
-    .then(() => doIt(4))
-    .then(() => doIt(8));
+const verbose = document.getElementById('verbose');
+const init = document.getElementById('init');
+const run = document.getElementById('run');
+const outputArea = document.getElementById('outputArea');
+
+run.disabled = true;
+
+const log = message => {
+    const oldText = outputArea.innerText;
+    const sep = oldText.length ? '\n' : '';
+    const newText = oldText + sep + message;
+    outputArea.innerHTML = newText;
+    return Promise.resolve();
+};
+
+console.log = log;
+console.info = log;
+
+init.addEventListener('click', e => {
+
+    e.preventDefault();
+
+    hamsters.init({
+        maxThreads: 8,
+        debug: verbose.checked ? 'verbose' : false
+    });
+
+    // 'hamsters.init' can only be called once.
+    verbose.disabled = true;
+    init.disabled = true;
+
+    run.disabled = false;
+});
+
+run.addEventListener('click', e => {
+
+    e.preventDefault();
+
+    log('Starting')
+        .then(() => run.disabled = true)
+        .then(() => doIt(2))
+        .then(() => doIt(4))
+        .then(() => doIt(8))
+        .then(() => log('Finished\n'))
+        .then(() => run.disabled = false);
+});
